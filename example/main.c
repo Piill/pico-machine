2abea1da50657157ddf716f9c818c1c6e8d0c60f
piill <pil@disroot.org>
2018-08-14 21:30:50 +0200

    Added support for memory mapped IO and custom error handlers.

--- example/main.c
+++ example/main.c
@@ -1,7 +1,7 @@
 #include "../core/core.h"
 #include <stdio.h>
 
-void print_status(struct machine*, int);
+void print_status(struct pmachine*, int);
 
 int main(int argc, char* argv[]) {
 	if(argc < 2) {
@@ -11,14 +11,14 @@ int main(int argc, char* argv[]) {
 
 	FILE* fp = fopen(argv[1], "r");
 	int res = 0;
-	char machine_code[0xFF];
+	char pmachine_code[0xFF];
 	for(int i = 0; i < 0xFF && res != EOF; i++) {
 		res = fgetc(fp);
-		machine_code[i] = res;
-		machine_code[i+1] = '\0';
+		pmachine_code[i] = res;
+		pmachine_code[i+1] = '\0';
 	}
 
-	struct machine* pico = create_machine(machine_code);
+	struct pmachine* pico = create_machine(pmachine_code);
 	for(;;) {
 		print_status(pico, 30);
 		execute_instruction(pico);
@@ -27,7 +27,7 @@ int main(int argc, char* argv[]) {
 }
 
 
-void print_status(struct machine* pico, int lines) {
+void print_status(struct pmachine* pico, int lines) {
 	printf("\033[2J\033[1;1H");
 	printf("addr\t content\t\t stack\n");
 	for(int i = 0; i < lines; i++) {
